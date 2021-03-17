a8aeebbfc108e0cbcb3ab28b0cd8381b7a8e2a86
Gayathri2109 <65301423+Gayathri2109@users.noreply.github.com>
2021-03-17 12:56:46 +0530

    Delete RMIServerAssignment/DataBase directory

--- RMIServerAssignment/DataBase/Invoice.java
+++
@@ -1,8 +0,0 @@
-package DataBase;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Invoice extends Remote{
-	public void startInvoiceApp(int i,int invNo)throws RemoteException;
-}
